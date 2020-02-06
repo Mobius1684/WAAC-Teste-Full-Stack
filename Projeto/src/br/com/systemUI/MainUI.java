23a19e78ccb018bf8eaa679364c8893d0b0bbc7e
Mobius1684 <dan_c_silva@hotmail.com>
2020-02-06 12:53:56 -0200

    Initial Project commit.

---
+++ Projeto/src/br/com/systemUI/MainUI.java
@@ -0,0 +1,348 @@
+package br.com.systemUI;
+
+import java.awt.EventQueue;
+import java.awt.BasicStroke;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.SpringLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.Panel;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+
+public class MainUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainUI window = new MainUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setAutoRequestFocus(false);
+		frame.setBounds(10, 10, 800, 600);
+		SpringLayout layout = new SpringLayout();
+		frame.getContentPane().setLayout(layout);
+		
+		JLabel LabelTitulo = new JLabel("N\u00FAmeros para o tri\u00E2ngulo ");
+		LabelTitulo.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		layout.putConstraint(SpringLayout.NORTH, LabelTitulo, 10, SpringLayout.NORTH, frame.getContentPane());
+		layout.putConstraint(SpringLayout.WEST, LabelTitulo, 258, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(LabelTitulo);
+		
+		JLabel lblPrimeiraLinha = new JLabel("Primeira Linha");
+		layout.putConstraint(SpringLayout.NORTH, lblPrimeiraLinha, 55, SpringLayout.NORTH, frame.getContentPane());
+		layout.putConstraint(SpringLayout.WEST, lblPrimeiraLinha, 10, SpringLayout.WEST, frame.getContentPane());
+		lblPrimeiraLinha.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		frame.getContentPane().add(lblPrimeiraLinha);
+		
+		JLabel lblSegundaLinha = new JLabel("Segunda Linha");
+		layout.putConstraint(SpringLayout.NORTH, lblSegundaLinha, 0, SpringLayout.NORTH, lblPrimeiraLinha);
+		layout.putConstraint(SpringLayout.WEST, lblSegundaLinha, 77, SpringLayout.EAST, lblPrimeiraLinha);
+		lblSegundaLinha.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		frame.getContentPane().add(lblSegundaLinha);
+		
+		JLabel lblTerceiraLinha = new JLabel("Terceira Linha");
+		layout.putConstraint(SpringLayout.NORTH, lblTerceiraLinha, 0, SpringLayout.NORTH, lblPrimeiraLinha);
+		layout.putConstraint(SpringLayout.EAST, lblTerceiraLinha, 0, SpringLayout.EAST, LabelTitulo);
+		lblTerceiraLinha.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		frame.getContentPane().add(lblTerceiraLinha);
+		
+		JComboBox comboBox1Linha1 = new JComboBox();
+		comboBox1Linha1.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		comboBox1Linha1.setMaximumRowCount(10);
+		layout.putConstraint(SpringLayout.NORTH, comboBox1Linha1, 27, SpringLayout.SOUTH, lblPrimeiraLinha);
+		layout.putConstraint(SpringLayout.WEST, comboBox1Linha1, 0, SpringLayout.WEST, lblPrimeiraLinha);
+		frame.getContentPane().add(comboBox1Linha1);
+		
+		JComboBox comboBox1Linha2 = new JComboBox();
+		comboBox1Linha2.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		comboBox1Linha2.setMaximumRowCount(10);
+		layout.putConstraint(SpringLayout.NORTH, comboBox1Linha2, 0, SpringLayout.NORTH, comboBox1Linha1);
+		layout.putConstraint(SpringLayout.WEST, comboBox1Linha2, 0, SpringLayout.WEST, lblSegundaLinha);
+		frame.getContentPane().add(comboBox1Linha2);
+		
+		JComboBox comboBox1Linha3 = new JComboBox();
+		comboBox1Linha3.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		comboBox1Linha3.setMaximumRowCount(10);
+		layout.putConstraint(SpringLayout.NORTH, comboBox1Linha3, 0, SpringLayout.NORTH, comboBox1Linha1);
+		layout.putConstraint(SpringLayout.EAST, comboBox1Linha3, -342, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().add(comboBox1Linha3);
+		
+		JLabel lblQuartaLinha = new JLabel("Quarta Linha");
+		layout.putConstraint(SpringLayout.NORTH, lblQuartaLinha, 0, SpringLayout.NORTH, lblPrimeiraLinha);
+		layout.putConstraint(SpringLayout.EAST, lblQuartaLinha, -63, SpringLayout.EAST, frame.getContentPane());
+		lblQuartaLinha.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		frame.getContentPane().add(lblQuartaLinha);
+		
+		JComboBox comboBox2Linha2 = new JComboBox();
+		comboBox2Linha2.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		comboBox2Linha2.setMaximumRowCount(10);
+		layout.putConstraint(SpringLayout.NORTH, comboBox2Linha2, 0, SpringLayout.NORTH, comboBox1Linha1);
+		layout.putConstraint(SpringLayout.WEST, comboBox2Linha2, 6, SpringLayout.EAST, comboBox1Linha2);
+		frame.getContentPane().add(comboBox2Linha2);
+		
+		JComboBox comboBox2Linha3 = new JComboBox();
+		comboBox2Linha3.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		comboBox2Linha3.setMaximumRowCount(10);
+		layout.putConstraint(SpringLayout.NORTH, comboBox2Linha3, 0, SpringLayout.NORTH, comboBox1Linha1);
+		layout.putConstraint(SpringLayout.WEST, comboBox2Linha3, 9, SpringLayout.EAST, comboBox1Linha3);
+		frame.getContentPane().add(comboBox2Linha3);
+		
+		JComboBox comboBox3Linha3 = new JComboBox();
+		comboBox3Linha3.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		comboBox3Linha3.setMaximumRowCount(10);
+		layout.putConstraint(SpringLayout.NORTH, comboBox3Linha3, 0, SpringLayout.NORTH, comboBox1Linha1);
+		layout.putConstraint(SpringLayout.WEST, comboBox3Linha3, 6, SpringLayout.EAST, comboBox2Linha3);
+		frame.getContentPane().add(comboBox3Linha3);
+		
+		JComboBox comboBox1Linha4 = new JComboBox();
+		comboBox1Linha4.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		comboBox1Linha4.setMaximumRowCount(10);
+		layout.putConstraint(SpringLayout.NORTH, comboBox1Linha4, 0, SpringLayout.NORTH, comboBox1Linha1);
+		layout.putConstraint(SpringLayout.WEST, comboBox1Linha4, 0, SpringLayout.WEST, lblQuartaLinha);
+		frame.getContentPane().add(comboBox1Linha4);
+		
+		JComboBox comboBox2Linha4 = new JComboBox();
+		comboBox2Linha4.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		comboBox2Linha4.setMaximumRowCount(10);
+		layout.putConstraint(SpringLayout.NORTH, comboBox2Linha4, 0, SpringLayout.NORTH, comboBox1Linha1);
+		layout.putConstraint(SpringLayout.WEST, comboBox2Linha4, 6, SpringLayout.EAST, comboBox1Linha4);
+		frame.getContentPane().add(comboBox2Linha4);
+		
+		JComboBox comboBox3Linha4 = new JComboBox();
+		comboBox3Linha4.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		comboBox3Linha4.setMaximumRowCount(10);
+		layout.putConstraint(SpringLayout.NORTH, comboBox3Linha4, 0, SpringLayout.NORTH, comboBox1Linha1);
+		layout.putConstraint(SpringLayout.WEST, comboBox3Linha4, 6, SpringLayout.EAST, comboBox2Linha4);
+		frame.getContentPane().add(comboBox3Linha4);
+		
+		JComboBox comboBox4Linha4 = new JComboBox();
+		comboBox4Linha4.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		comboBox4Linha4.setMaximumRowCount(10);
+		layout.putConstraint(SpringLayout.NORTH, comboBox4Linha4, 0, SpringLayout.NORTH, comboBox1Linha1);
+		layout.putConstraint(SpringLayout.WEST, comboBox4Linha4, 6, SpringLayout.EAST, comboBox3Linha4);
+		frame.getContentPane().add(comboBox4Linha4);
+		
+		JButton BotaoAplicar = new JButton("Aplicar");
+		layout.putConstraint(SpringLayout.WEST, BotaoAplicar, 0, SpringLayout.WEST, comboBox3Linha4);
+		frame.getContentPane().add(BotaoAplicar);
+		
+		JLabel lblX1_1 = new JLabel("XX");
+		layout.putConstraint(SpringLayout.WEST, lblX1_1, 256, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(lblX1_1);
+		
+		JLabel lblX2_1 = new JLabel("XX");
+		layout.putConstraint(SpringLayout.NORTH, lblX2_1, 180, SpringLayout.SOUTH, comboBox1Linha2);
+		layout.putConstraint(SpringLayout.WEST, lblX2_1, 197, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(lblX2_1);
+		
+		JLabel lblX2_2 = new JLabel("XX");
+		layout.putConstraint(SpringLayout.NORTH, lblX2_2, -2, SpringLayout.NORTH, lblX2_1);
+		layout.putConstraint(SpringLayout.WEST, lblX2_2, 298, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(lblX2_2);
+		
+		JLabel lblX3_1 = new JLabel("XX");
+		layout.putConstraint(SpringLayout.WEST, lblX3_1, 160, SpringLayout.WEST, frame.getContentPane());
+		layout.putConstraint(SpringLayout.EAST, lblX3_1, -606, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().add(lblX3_1);
+		
+		JLabel lblX3_2 = new JLabel("XX");
+		layout.putConstraint(SpringLayout.WEST, lblX3_2, 258, SpringLayout.WEST, frame.getContentPane());
+		layout.putConstraint(SpringLayout.SOUTH, lblX1_1, -53, SpringLayout.NORTH, lblX3_2);
+		layout.putConstraint(SpringLayout.NORTH, lblX3_2, 0, SpringLayout.NORTH, lblX3_1);
+		frame.getContentPane().add(lblX3_2);
+		
+		JLabel lblX3_3 = new JLabel("XX");
+		layout.putConstraint(SpringLayout.WEST, lblX3_3, 340, SpringLayout.WEST, frame.getContentPane());
+		layout.putConstraint(SpringLayout.EAST, lblX3_3, -426, SpringLayout.EAST, frame.getContentPane());
+		layout.putConstraint(SpringLayout.NORTH, lblX3_1, 0, SpringLayout.NORTH, lblX3_3);
+		frame.getContentPane().add(lblX3_3);
+		
+		JLabel lblX4_1 = new JLabel("XX");
+		layout.putConstraint(SpringLayout.NORTH, BotaoAplicar, -4, SpringLayout.NORTH, lblX4_1);
+		layout.putConstraint(SpringLayout.WEST, lblX4_1, 119, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(lblX4_1);
+		
+		JLabel lblX4_2 = new JLabel("XX");
+		layout.putConstraint(SpringLayout.SOUTH, lblX3_1, -25, SpringLayout.NORTH, lblX4_2);
+		layout.putConstraint(SpringLayout.SOUTH, lblX3_2, -25, SpringLayout.NORTH, lblX4_2);
+		layout.putConstraint(SpringLayout.SOUTH, lblX3_3, -25, SpringLayout.NORTH, lblX4_2);
+		layout.putConstraint(SpringLayout.SOUTH, lblX2_2, -63, SpringLayout.NORTH, lblX4_2);
+		layout.putConstraint(SpringLayout.SOUTH, lblX2_1, -64, SpringLayout.NORTH, lblX4_2);
+		layout.putConstraint(SpringLayout.WEST, lblX4_2, 197, SpringLayout.WEST, frame.getContentPane());
+		layout.putConstraint(SpringLayout.NORTH, lblX4_1, 0, SpringLayout.NORTH, lblX4_2);
+		layout.putConstraint(SpringLayout.EAST, lblX4_1, -62, SpringLayout.WEST, lblX4_2);
+		frame.getContentPane().add(lblX4_2);
+		
+		JLabel lblX4_3 = new JLabel("XX");
+		layout.putConstraint(SpringLayout.NORTH, lblX4_3, 383, SpringLayout.NORTH, frame.getContentPane());
+		layout.putConstraint(SpringLayout.WEST, lblX4_3, 91, SpringLayout.EAST, lblX4_2);
+		layout.putConstraint(SpringLayout.NORTH, lblX4_2, 0, SpringLayout.NORTH, lblX4_3);
+		frame.getContentPane().add(lblX4_3);
+		
+		JLabel lblX4_4 = new JLabel("XX");
+		layout.putConstraint(SpringLayout.NORTH, lblX4_4, 0, SpringLayout.NORTH, lblX4_3);
+		layout.putConstraint(SpringLayout.WEST, lblX4_4, 64, SpringLayout.EAST, lblX4_3);
+		frame.getContentPane().add(lblX4_4);
+		
+		JLabel lblSomaFormada = new JLabel("A soma formada entre os maiores n\u00FAmeros \u00E9:");
+		layout.putConstraint(SpringLayout.SOUTH, lblSomaFormada, -49, SpringLayout.SOUTH, frame.getContentPane());
+		layout.putConstraint(SpringLayout.EAST, lblSomaFormada, 0, SpringLayout.EAST, comboBox1Linha3);
+		lblSomaFormada.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		frame.getContentPane().add(lblSomaFormada);
+		
+		Panel panel1_1 = new Panel();
+		panel1_1.setBackground(Color.BLACK);
+		layout.putConstraint(SpringLayout.NORTH, panel1_1, 160, SpringLayout.SOUTH, comboBox1Linha2);
+		layout.putConstraint(SpringLayout.WEST, panel1_1, -44, SpringLayout.WEST, lblX1_1);
+		layout.putConstraint(SpringLayout.SOUTH, panel1_1, 176, SpringLayout.SOUTH, comboBox1Linha2);
+		layout.putConstraint(SpringLayout.EAST, panel1_1, -6, SpringLayout.WEST, lblX1_1);
+		//Graphics line1_1 = panel1_1.getGraphics();
+		//line1_1.drawLine(0, 0, 200, 200);
+		panel1_1.addComponentListener();
+		frame.getContentPane().add(panel1_1);
+		
+		
+		Panel panel1_2 = new Panel();
+		layout.putConstraint(SpringLayout.NORTH, panel1_2, 0, SpringLayout.NORTH, panel1_1);
+		layout.putConstraint(SpringLayout.WEST, panel1_2, 6, SpringLayout.EAST, lblX1_1);
+		layout.putConstraint(SpringLayout.SOUTH, panel1_2, 0, SpringLayout.SOUTH, panel1_1);
+		layout.putConstraint(SpringLayout.EAST, panel1_2, 0, SpringLayout.EAST, lblSegundaLinha);
+		panel1_2.setBackground(Color.BLACK);
+		//Graphics line1_2 = panel1_2.getGraphics();
+		//line1_2.drawLine(0, 0, 200, 200);
+		frame.getContentPane().add(panel1_2);
+		
+		Panel panel2_1 = new Panel();
+		layout.putConstraint(SpringLayout.NORTH, panel2_1, 6, SpringLayout.SOUTH, lblX2_1);
+		layout.putConstraint(SpringLayout.WEST, panel2_1, 0, SpringLayout.WEST, lblX3_1);
+		layout.putConstraint(SpringLayout.SOUTH, panel2_1, -1, SpringLayout.NORTH, lblX3_1);
+		layout.putConstraint(SpringLayout.EAST, panel2_1, -15, SpringLayout.EAST, lblX2_1);
+		panel2_1.setBackground(Color.BLACK);
+		//Graphics line2_1 = panel2_1.getGraphics();
+		//line2_1.drawLine(0, 0, 200, 200);
+		frame.getContentPane().add(panel2_1);
+		
+		Panel panel2_2 = new Panel();
+		layout.putConstraint(SpringLayout.NORTH, panel2_2, 0, SpringLayout.NORTH, panel2_1);
+		layout.putConstraint(SpringLayout.WEST, panel2_2, 0, SpringLayout.WEST, panel1_1);
+		layout.putConstraint(SpringLayout.SOUTH, panel2_2, -1, SpringLayout.NORTH, lblX3_2);
+		layout.putConstraint(SpringLayout.EAST, panel2_2, -524, SpringLayout.EAST, frame.getContentPane());
+		panel2_2.setBackground(Color.BLACK);
+		//Graphics line2_2 = panel2_2.getGraphics();
+		//line2_2.drawLine(0, 0, 200, 200);
+		frame.getContentPane().add(panel2_2);
+		
+		Panel panel2_3 = new Panel();
+		layout.putConstraint(SpringLayout.NORTH, panel2_3, 6, SpringLayout.SOUTH, lblX2_2);
+		layout.putConstraint(SpringLayout.WEST, panel2_3, -4, SpringLayout.WEST, panel1_2);
+		layout.putConstraint(SpringLayout.SOUTH, panel2_3, 1, SpringLayout.SOUTH, panel2_1);
+		panel2_3.setBackground(Color.BLACK);
+		//Graphics line2_3 = panel2_3.getGraphics();
+		//line2_3.drawLine(0, 0, 200, 200);
+		frame.getContentPane().add(panel2_3);
+		
+		Panel panel2_4 = new Panel();
+		layout.putConstraint(SpringLayout.NORTH, panel2_4, 0, SpringLayout.NORTH, panel2_1);
+		layout.putConstraint(SpringLayout.WEST, panel2_4, 8, SpringLayout.EAST, panel2_3);
+		layout.putConstraint(SpringLayout.SOUTH, panel2_4, 0, SpringLayout.SOUTH, panel2_1);
+		layout.putConstraint(SpringLayout.EAST, panel2_4, -426, SpringLayout.EAST, frame.getContentPane());
+		panel2_4.setBackground(Color.BLACK);
+		//Graphics line2_4 = panel2_4.getGraphics();
+		//line2_4.drawLine(0, 0, 200, 200);
+		frame.getContentPane().add(panel2_4);
+		
+		Panel panel3_1 = new Panel();
+		layout.putConstraint(SpringLayout.NORTH, panel3_1, 6, SpringLayout.SOUTH, lblX3_1);
+		layout.putConstraint(SpringLayout.WEST, panel3_1, 0, SpringLayout.WEST, lblX4_1);
+		layout.putConstraint(SpringLayout.SOUTH, panel3_1, 22, SpringLayout.SOUTH, lblX3_1);
+		layout.putConstraint(SpringLayout.EAST, panel3_1, 38, SpringLayout.WEST, lblX4_1);
+		panel3_1.setBackground(Color.BLACK);
+		//Graphics line3_1 = panel3_1.getGraphics();
+		//line3_1.drawLine(0, 0, 200, 200);
+		frame.getContentPane().add(panel3_1);
+		
+		Panel panel3_2 = new Panel();
+		layout.putConstraint(SpringLayout.NORTH, panel3_2, 6, SpringLayout.SOUTH, lblX3_1);
+		layout.putConstraint(SpringLayout.WEST, panel3_2, -34, SpringLayout.EAST, lblX2_1);
+		layout.putConstraint(SpringLayout.SOUTH, panel3_2, 0, SpringLayout.SOUTH, panel3_1);
+		layout.putConstraint(SpringLayout.EAST, panel3_2, 0, SpringLayout.EAST, lblX2_1);
+		panel3_2.setBackground(Color.BLACK);
+		//Graphics line3_2 = panel3_2.getGraphics();
+		//line3_2.drawLine(0, 0, 200, 200);
+		frame.getContentPane().add(panel3_2);
+		
+		Panel panel3_3 = new Panel();
+		layout.putConstraint(SpringLayout.NORTH, panel3_3, 6, SpringLayout.SOUTH, lblX3_2);
+		layout.putConstraint(SpringLayout.WEST, panel3_3, 18, SpringLayout.EAST, panel3_2);
+		layout.putConstraint(SpringLayout.SOUTH, panel3_3, 0, SpringLayout.SOUTH, panel3_1);
+		panel3_3.setBackground(Color.BLACK);
+		//Graphics line3_3 = panel3_3.getGraphics();
+		//line3_3.drawLine(0, 0, 200, 200);
+		frame.getContentPane().add(panel3_3);
+		
+		Panel panel3_4 = new Panel();
+		layout.putConstraint(SpringLayout.EAST, panel2_3, 0, SpringLayout.EAST, panel3_4);
+		layout.putConstraint(SpringLayout.EAST, panel3_3, -13, SpringLayout.WEST, panel3_4);
+		layout.putConstraint(SpringLayout.NORTH, panel3_4, 0, SpringLayout.NORTH, panel3_1);
+		layout.putConstraint(SpringLayout.WEST, panel3_4, 0, SpringLayout.WEST, panel1_2);
+		layout.putConstraint(SpringLayout.SOUTH, panel3_4, 0, SpringLayout.SOUTH, panel3_1);
+		layout.putConstraint(SpringLayout.EAST, panel3_4, -8, SpringLayout.EAST, lblX4_3);
+		panel3_4.setBackground(Color.BLACK);
+		//Graphics line3_4 = panel3_4.getGraphics();
+		//line3_4.drawLine(0, 0, 200, 200);
+		frame.getContentPane().add(panel3_4);
+		
+		Panel panel3_5 = new Panel();
+		layout.putConstraint(SpringLayout.NORTH, panel3_5, 6, SpringLayout.SOUTH, lblX3_3);
+		layout.putConstraint(SpringLayout.WEST, panel3_5, 13, SpringLayout.EAST, panel3_4);
+		layout.putConstraint(SpringLayout.SOUTH, panel3_5, 0, SpringLayout.SOUTH, panel3_1);
+		panel3_5.setBackground(Color.BLACK);
+		//Graphics line3_5 = panel3_5.getGraphics();
+		//line3_5.drawLine(0, 0, 200, 200);
+		frame.getContentPane().add(panel3_5);
+		
+		Panel panel3_6 = new Panel();
+		layout.putConstraint(SpringLayout.EAST, panel3_5, -10, SpringLayout.WEST, panel3_6);
+		layout.putConstraint(SpringLayout.SOUTH, panel3_6, 0, SpringLayout.SOUTH, panel3_1);
+		layout.putConstraint(SpringLayout.EAST, panel3_6, 7, SpringLayout.EAST, lblX4_4);
+		layout.putConstraint(SpringLayout.NORTH, panel3_6, 0, SpringLayout.NORTH, panel3_1);
+		layout.putConstraint(SpringLayout.WEST, panel3_6, -27, SpringLayout.EAST, lblX4_4);
+		panel3_6.setBackground(Color.BLACK);
+		//Graphics line3_6 = panel3_6.getGraphics();
+		//line3_6.drawLine(0, 0, 200, 200);
+		frame.getContentPane().add(panel3_6);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
